03314fc9eac9293a9b8afd168f4eb0c19cabf6fd
youzi <muyiluling@gmail.com>
2026-04-09 16:27:19 +0800

    Add files via upload

--- api/conviction.js
+++ api/conviction.js
@@ -14,6 +14,13 @@ async function getCollection() {
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
 
   const date = req.query.date || new Date().toISOString().slice(0, 10);
